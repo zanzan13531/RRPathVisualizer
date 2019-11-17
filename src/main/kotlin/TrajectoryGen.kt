7505b942dab2300eddea32f745293607e9ad6a3c
technova0 <yjw558@gmail.com>
2019-11-17 06:39:31 -0800

    change paths

--- src/main/kotlin/TrajectoryGen.kt
+++ src/main/kotlin/TrajectoryGen.kt
@@ -8,15 +8,19 @@ import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
 import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints
 
 object TrajectoryGen {
-    private val constraints = DriveConstraints(36.0, 30.0, 0.0, 180.0.toRadians, 180.0.toRadians, 0.0)
-    private val startPose = Pose2d(-33.0, -63.0, 180.0.toRadians)
+    private val constraints = DriveConstraints(42.0, 30.0, 0.0, 180.0.toRadians, 180.0.toRadians, 0.0)
+
 
     fun createTrajectory(): ArrayList<Trajectory> {
         val list = ArrayList<Trajectory>()
 
+        val startPose = Pose2d(-33.0, -63.0, 180.0.toRadians)
+        //val startPose = Pose2d(50.0, -35.0, 180.0.toRadians)
+//        val startPose = Pose2d(-18.0, -35.0, 180.0.toRadians)
         val builder1 = TrajectoryBuilder(startPose, constraints)
 
-        builder1.splineTo(Pose2d(-59.0,-36.0, 180.0.toRadians))
+        builder1.strafeRight(25.75)
+            .forward(28.0)
             .reverse()
             .splineTo(Pose2d(0.0,-41.0, 180.0.toRadians))
             .splineTo(Pose2d(50.0,-36.0, 180.0.toRadians))
@@ -27,10 +31,10 @@ object TrajectoryGen {
             .splineTo(Pose2d(0.0,-41.0, 180.0.toRadians))
             .splineTo(Pose2d(50.0,-36.0, 180.0.toRadians))
             .reverse()
-            .splineTo(Pose2d(0.0,-39.0, 180.0.toRadians))
+            .splineTo(Pose2d(8.0,-42.0, 180.0.toRadians))
             .splineTo(Pose2d(-18.0,-36.0, 180.0.toRadians))
             .reverse()
-            .splineTo(Pose2d(0.0,-41.0, 180.0.toRadians))
+            .splineTo(Pose2d(5.0,-42.0, 180.0.toRadians))
             .splineTo(Pose2d(52.0,-35.0, 180.0.toRadians))
             .reverse()
             .splineTo(Pose2d(0.0,-39.0, 180.0.toRadians))
@@ -46,6 +50,14 @@ object TrajectoryGen {
             .splineTo(Pose2d(48.0,-56.0, 0.0.toRadians))
             .reverse()
             .splineTo(Pose2d(0.0,-41.0, 0.0.toRadians))
+
+
+        builder1
+    //        .splineTo(Pose2d(8.0,-42.0, 180.0.toRadians))
+    //        .splineTo(Pose2d(-18.0,-36.0, 180.0.toRadians))
+//            .reverse()
+//            .splineTo(Pose2d(3.0,-44.0, 180.0.toRadians))
+//            .splineTo(Pose2d(50.0,-35.0, 180.0.toRadians))
 
 
         // dump routine
@@ -72,6 +84,8 @@ object TrajectoryGen {
 
     fun drawOffbounds() {
         GraphicsUtil.fillRect(Vector2d(-12.0, -63.0), 18.0, 18.0) // robot against the wall
+
+        GraphicsUtil.fillRect(Vector2d(0.0, -24.0), 12.0, 8.0) // robot against the wall
     }
 }
 
